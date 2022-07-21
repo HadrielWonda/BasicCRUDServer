9b706ff76480af6fb85a1c2a91e3b3acae655735
Olawale Abaniwonda <olawalebash2017@gmail.com>
2022-07-21 01:02:37 +0100

    Add files via upload

---
+++ src/Client/State.fs
@@ -0,0 +1,105 @@
+﻿module Client.State
+
+open Client.Types
+open Elmish
+open Shared 
+
+let initialState() = 
+    let initState =
+        {
+            Users    = Map.empty
+            NewUser  = None
+            Error    = None
+            PrevUser = None
+        }
+
+    initState, Cmd.ofMsg LoadUsers
+
+let findUserIndexById (users : UserModel array) (id : UserId) =
+    users |> Array.findIndex (fun x -> x.Id = id)
+
+let update (msg: Msg) (prevState: State) = 
+    match msg with
+    | SetNewUserFirstName name ->
+        let nextState = prevState.NewUser
+                        |> Option.map (fun x -> {x with FirstName = name})
+                        |> Option.defaultValue { Id = 0; FirstName = name; LastName = "" }
+                        |> fun x -> { prevState with NewUser = Some x; Error = None }
+        nextState, Cmd.none
+
+    | SetNewUserLastName name ->
+        let nextState = prevState.NewUser
+                        |> Option.map (fun x -> {x with LastName = name})
+                        |> Option.defaultValue { Id = 0; FirstName = ""; LastName = name }
+                        |> fun x -> { prevState with NewUser = Some x; Error = None }
+        nextState, Cmd.none
+
+    | LoadUsers ->
+        prevState, Server.loadUsers()
+
+    | AddUser ->
+        match prevState.NewUser with
+        | None -> prevState, Cmd.none
+        | Some user -> prevState, Server.addUser user
+
+    | UserAdded user ->
+        let nextUsers = prevState.Users |> Map.add user.Id (UserModel.Create user)
+        let nextState = { prevState with Users = nextUsers; NewUser = None; Error = None }
+        nextState, Cmd.none
+            
+    | UsersLoaded users ->
+        let nextUsers = users |> List.map (fun x -> x.Id, UserModel.Create x) |> Map.ofList
+        let nextState = { prevState with Users = nextUsers; Error = None}
+        nextState, Cmd.none
+    
+    | UpdateUser user ->
+        prevState, Server.updateUser user
+    
+    | UserUpdated user ->
+        let nextUsers = prevState.Users |> Map.add user.Id (UserModel.Create user)
+        let nextState = { prevState with Users = nextUsers; Error = None }
+        nextState, Cmd.none
+
+    | DeleteUser userId ->
+        prevState, Server.deleteUser userId
+
+    | UserDeleted user ->
+        let nextUsers = prevState.Users |> Map.remove user.Id
+        let nextState = { prevState with Users = nextUsers; Error = None }
+        nextState, Cmd.none
+
+    | UpdateRowState (id : UserId, state : RowState) ->
+        let getUpdatedUser (user : UserModel) =
+            if state = RowState.HasEdit then
+                 Some user, { user with RowState = state }
+            else None, prevState.PrevUser.Value
+
+        let prevUser, newUser = getUpdatedUser prevState.Users.[id]
+
+        let nextUsers = prevState.Users |> Map.add id newUser
+        let nextState = { prevState with Users = nextUsers; Error = None; PrevUser = prevUser }
+        nextState, Cmd.none
+
+    | UpdateRowName (id : UserId, name : string, isFirstName : bool) ->
+        let userUpdated (x : UserModel) =
+            if isFirstName then
+                 { x with FirstName = name }
+            else { x with LastName = name }
+
+        let userUpdated = userUpdated prevState.Users.[id]
+
+        let nextUsers = prevState.Users |> Map.add id userUpdated
+        let nextState = { prevState with Users = nextUsers; Error = None }
+        nextState, Cmd.none
+
+    | LoadUsersFailure er ->
+        let nextState = { prevState with Error = Some er }
+        nextState, Cmd.none
+
+    | AddUserFailed er ->
+        let nextState = { prevState with Error = Some er }
+        nextState, Cmd.none
+
+    | UpdateUserFailed er | DeleteUserFailure er ->
+        let nextState = { prevState with Error = Some er }
+        nextState, Cmd.none
---
+++ src/Client/View.fs
@@ -0,0 +1,101 @@
+﻿module Client.View
+
+open Browser.Types
+open Shared
+open Client.Types
+open Fable.React
+open Fable.React.Props
+
+let widthButton = 100
+
+let isDangerId (error : ErrorMessage option) (id : UserId) =
+    match error with
+    | Some x -> x.Id = id
+    | None _ -> false
+
+let divFieldUser =
+    div [ ClassName "field is-grouped is-grouped-centered" ]
+let divInputClass =
+    div [ ClassName "control is-normal" ]
+
+let errorMessage (error : string) =
+    [ divFieldUser
+        [ span [ Style [ Color "red" ] ]
+               [ str error ]] ]
+
+let renderButton (strButton : string) (color : string) (clickEventFunc : MouseEvent -> unit) =
+    let classname = sprintf "button is-normal %s" color
+    [ button [ ClassName classname
+               Style [ Width widthButton ]
+               OnClick clickEventFunc ]
+             [ str strButton ] ]
+
+let renderInputNameReadonly (name : string) =
+    [ input [ ReadOnly true
+              ClassName "input is-normal"
+              Value name ] ]
+
+let renderInputName (placeHolder : string) (value : string) (changeEventFunc : Event -> unit) (isDanger : bool) =
+    [ input [ ClassName ("input is-normal" + if isDanger then " is-danger" else "")
+              Placeholder placeHolder
+              DefaultValue value
+              Value value
+              OnChange changeEventFunc] ]
+
+
+let renderUser (user: UserModel) (error : ErrorMessage option) (dispatch : Msg -> unit) =
+    let isDanger = isDangerId error user.Id
+    let firstInput =
+        if user.RowState = RowState.HasEdit then
+             renderInputName "" user.FirstName (fun ev -> (user.Id, ev.Value, true) |> UpdateRowName |> dispatch) isDanger
+        else renderInputNameReadonly user.FirstName
+    let lastInput =
+        if user.RowState = RowState.HasEdit then
+             renderInputName "" user.LastName (fun ev -> (user.Id, ev.Value, false) |> UpdateRowName |> dispatch) isDanger
+        else renderInputNameReadonly user.LastName
+    let firstButton =
+        if user.RowState = RowState.HasEdit then
+             renderButton "Cancel" "is-link"    (fun _ -> (user.Id, RowState.HasNormal) |> UpdateRowState |> dispatch)
+        else renderButton "Edit"   "is-warning" (fun _ -> (user.Id, RowState.HasEdit)   |> UpdateRowState |> dispatch)
+    let secondButton =
+        if user.RowState = RowState.HasEdit then
+             renderButton "Update" "is-danger" (fun _ -> user    |> UpdateUser |> dispatch)
+        else renderButton "Delete" "is-danger" (fun _ -> user.Id |> DeleteUser |> dispatch)
+    tr [] [
+         td [] [ divInputClass firstInput   ]
+         td [] [ divInputClass lastInput    ]
+         td [] [ divInputClass firstButton  ]
+         td [] [ divInputClass secondButton ] ]
+
+
+let addUser (state: State) (dispatch : Msg -> unit) =
+    let isDanger = isDangerId state.Error -1
+    let textValue = defaultArg state.NewUser {Id=0; FirstName = ""; LastName = ""}
+    let firstInput =
+        renderInputName "First Name" textValue.FirstName (fun ev -> ev.Value |> SetNewUserFirstName |> dispatch) isDanger
+    let lastInput =
+        renderInputName "Last Name"  textValue.LastName  (fun ev -> ev.Value |> SetNewUserLastName  |> dispatch) isDanger
+    divFieldUser
+        [
+            divInputClass firstInput
+            divInputClass lastInput
+            divInputClass
+                [ button [ ClassName "button is-normal is-success"
+                           Style [ Width widthButton ]
+                           OnClick (fun _ -> dispatch AddUser) ]
+                         [ str "Add" ] ] ]
+
+let render (state: State) (dispatch : Msg -> unit) =
+    let rows = state.Users |> Seq.collect (fun kv -> [renderUser kv.Value state.Error dispatch])
+    div
+        [ Style [ Padding 20 ] ]
+        [ yield! (if state.Error.IsSome then (errorMessage state.Error.Value.Message) else [])
+          div [ ClassName "box" ] [ addUser state dispatch ]
+          div
+              [ ClassName "box"
+                Style [ TextAlign TextAlignOptions.Center ] ]
+              [ table [ ClassName "table"
+                        Style [ Display DisplayOptions.InlineBlock ] ]
+                      [
+                        thead [] [tr [] [th [] [str "First Name"]; th [] [str "Last Name"]; th [] []; th [] []]]
+                        tbody [] rows ]]]
